cb9248de6d83e950daf4a22cd2bbb9c932cb844c
SAlexS <alex171293@hotmail.com>
2020-05-05 13:44:03 +0200

    powershell intial file

---
+++ Week_1/WincDag_2/script.js
@@ -0,0 +1,18 @@
+console.log("Hello Winc Academy");
+
+let name = "Alex van den Bos";
+console.log(name);
+
+let bankRek = 4000;
+let uitGave = 1000;
+
+let begroting = bankRek - uitGave;
+console.log(begroting);
+
+console.log(typeof(begroting));
+console.log(typeof(name));
+//Dit is een grote som
+/*Dit is staat uit.
+console.log(1230941*1823794)
+console.log(637263/54)
+*/
