ded9f1c06ec6f8acd57ceb59bed43aa06f8c4521
SAlexS <alex171293@hotmail.com>
2020-05-05 10:05:39 +0200

    html en js playground intial file

---
+++ Week_1/WincDag_2/javascr.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+
+let name = "Alex van den Bos";
+console.log(name);
+
+let bankRek = 4000;
+let uitGave = 1000;
+
+let begroting = bankRek - uitGave;
+console.log(begroting);
+
+console.log(typeof(begroting));
+console.log(typeof(name));
