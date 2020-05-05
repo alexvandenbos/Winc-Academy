9e6345f67a710852d64ff9ffd235adf2fce0ab2a
SAlexS <alex171293@hotmail.com>
2020-05-05 14:14:11 +0200

    else if statement intial file dag 2 opdr 3

---
+++ Week_1/WincDag_2/javascrIfElseStatement.js
@@ -0,0 +1,22 @@
+const age = 18;
+const isFemal = true;
+const driverStatus = "bob";
+
+if (age >= 18) {
+    console.log("Je mag naar binnen, je bent 18 of ouder");
+} else {
+    console.log("je mag niet naar binnen, je bent jonger dan 18");
+}
+
+if (isFemal === true) {
+    console.log("Voor jou is het eerste drankje gratis");
+} else {
+    console.log("Voor jou is alles duurder");
+}
+
+if (driverStatus === "bob") {
+    console.log("Jij mag de naar huis rijden, je bent bob");
+} else {
+    console.log("Jij mag niet naar huis rijden, je bent onder invloed");
+    
+}
