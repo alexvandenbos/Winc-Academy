233ee4aceb47bf2b2123b2888ae5950eb5a740db
alex van den Bos <alex171293@hotmail.com>
2020-05-24 14:32:25 +0200

    basis eindopracht, post  & delete functie initial commit

--- Week_3/WindDag_5/eindopdracht/api-client.js
+++ Week_3/WindDag_5/eindopdracht/api-client.js
@@ -5,10 +5,37 @@ const getData = async () => {
         const res = await fetch(apiURL, {method: "GET"});
         const data = await res.json();
         console.log("The getData function has retrieved API data");
-        //console.log(data)
         return data
     } catch (err) {
         console.log(err);
         return error
     }
 };
+
+const postData = async () => {
+    let taskdescription = document.getElementById('to-do-task-id').value
+    console.log(`Ik post: ${taskdescription}`)
+    const apiURL = `https://wincacademydatabase.firebaseio.com/${API_KEY}/Tasks.json`
+    let response = await fetch(apiURL, {
+        method: 'POST', 
+        headers: {'Content-Type': 'application/json;charset=utf-8'},
+        body: JSON.stringify({
+            "description": taskdescription, 
+            "done": false
+        })
+    })
+    console.log(response)
+    emptyList()
+    result()
+};
+
+const deleteData = async (taskid) => {
+    const apiURL = `https://wincacademydatabase.firebaseio.com/${API_KEY}/Tasks/${taskid}.json`
+    let response = await fetch(apiURL, {
+        method: 'DELETE', 
+        headers: {'Content-Type': 'application/json;charset=utf-8'},
+    })
+    console.log(response)
+    emptyList()
+    result()
+}
--- Week_3/WindDag_5/eindopdracht/script.js
+++ Week_3/WindDag_5/eindopdracht/script.js
@@ -1,17 +1,36 @@
-// const result = async function() {
-//     const data = await getData();    
-//     return data
-// }
-const result = getData().then((value) => {
-    console.log(value)
-    pushListItemToDom(value)
-    return value
-});
+const result = () => { 
+    getData().then((taskData) => {
+        console.log("Before (the raw result):", taskData);
+        let tasks = Object.keys(taskData).map(key => ({
+            id: key,
+            description: taskData[key].description,
+            done: taskData[key].done
+        }));
+        console.log("After the tasks array", tasks);    
+        tasks.forEach(task => {
+            pushListItemToDom(task.description, task.id)
+        })
+    }); 
+}
 
 //maakt list-items aan met tasks
-const pushListItemToDom = (task) => {    
+const pushListItemToDom = (task, taskid) => {    
     const listLocation = document.querySelector('#opdrachten-list')   
     let newLi = document.createElement("li")
-    newLi.innerHTML = task;    
+    let newImg = document.createElement("img")
+    newImg.setAttribute('src', 'trash-delete-icon.jpg');
+    newImg.setAttribute('alt', 'delete icon');
+    newImg.setAttribute('height', '15px');
+    newImg.setAttribute('width', '15px');
+    newImg.setAttribute('onclick', `deleteData("${taskid}");`);
+    newLi.innerHTML = task;
+    newLi.appendChild(newImg)    
     listLocation.appendChild(newLi)   
 };
+
+//maakt de lijst leeg
+const emptyList = () => {
+    const listLocation = document.querySelector('#opdrachten-list')
+    listLocation.innerHTML = ""            
+};
+result()
