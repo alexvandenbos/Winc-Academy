d4a0b27a87e4f1767cd01f17e0fbf217879b06d6
alex van den Bos <alex171293@hotmail.com>
2020-05-28 20:50:35 +0200

    linter en syntax automation. gitignore update

--- Week_4/WincDag-4/TDD/functions.test.js
+++ Week_4/WincDag-4/TDD/functions.test.js
@@ -1,38 +1,38 @@
 // const addOne = require("./addone.js")
 
 // test("Add 1 to each item in myArray", function() {
-//     const myArray = [31, 57, 12, 5];  
+//     const myArray = [31, 57, 12, 5];
 //     const unchanged = [31, 57, 12, 5];
-    
-//     const expected = [32, 58, 13, 6];    
+
+//     const expected = [32, 58, 13, 6];
 //     const output = addOne(myArray);
-  
+
 //     expect(output).toEqual(expected);
 //     expect(myArray).toEqual(unchanged);
 //   });
-const functions = require('./functions.js')
+const functions = require("./functions.js");
 
-test("Add 1 to each item in myArray", function() {
-    const myArray = [31, 57, 12, 5];  
-    const unchanged = [31, 57, 12, 5];
-    
-    const expected = [32, 58, 13, 6];    
-    const output = functions.addOne(myArray);
-  
-    expect(output).toEqual(expected);
-    expect(myArray).toEqual(unchanged);
-});
-test("Display the length of the words from array words", function() {
-    const words = ["sun", "potato", "roundabout", "pizza"];
-    const expected = [3, 6, 10, 5];
+test("Add 1 to each item in myArray", function () {
+  const myArray = [31, 57, 12, 5];
+  const unchanged = [31, 57, 12, 5];
 
-    const output = functions.getWordLengths(words);
-    expect(output).toEqual(expected);
+  const expected = [32, 58, 13, 6];
+  const output = functions.addOne(myArray);
+
+  expect(output).toEqual(expected);
+  expect(myArray).toEqual(unchanged);
 });
-test("Find the needle", function() {
-    const words = ["house", "train", "slide", "needle", "book"];
-    const expected = 3;
-  
-    const output = functions.findNeedle(words, "needle");
-    expect(output).toEqual(expected);
-  });
+test("Display the length of the words from array words", function () {
+  const words = ["sun", "potato", "roundabout", "pizza"];
+  const expected = [3, 6, 10, 5];
+
+  const output = functions.getWordLengths(words);
+  expect(output).toEqual(expected);
+});
+test("Find the needle", function () {
+  const words = ["house", "train", "slide", "needle", "book"];
+  const expected = 3;
+
+  const output = functions.findNeedle(words, "needle");
+  expect(output).toEqual(expected);
+});
--- Week_4/WincDag-4/jestopdracht/functions.test.js
+++ Week_4/WincDag-4/jestopdracht/functions.test.js
@@ -1,28 +1,24 @@
-const functions = require('./functions.js')
+const functions = require("./functions.js");
 
-test('Should be null', () => {
+test("Should be null", () => {
   expect(functions.isNull()).toBeNull();
 });
 
-test('checkValue Should be falsy when argument is undefined', () => {
+test("checkValue Should be falsy when argument is undefined", () => {});
 
-});
+test("User should be Brad Traversy object", () => {});
 
-test('User should be Brad Traversy object', () => {
-
-});
-
-test('Should be under or equal to 1600', () => {
-	const load1 = 800;
+test("Should be under or equal to 1600", () => {
+  const load1 = 800;
   const load2 = 800;
-	expect(load1 + load2).toBe(load1+load2)
+  expect(load1 + load2).toBe(load1 + load2);
 });
 
-test('There is no I in team', () => {
-	expect('team').not.toContain('I');
+test("There is no I in team", () => {
+  expect("team").not.toContain("I");
 });
 
-test('Admin should be in usernames', () => {
-  usernames = ['john', 'karen', 'admin'];
-  expect(usernames).toContain('admin');
+test("Admin should be in usernames", () => {
+  usernames = ["john", "karen", "admin"];
+  expect(usernames).toContain("admin");
 });
