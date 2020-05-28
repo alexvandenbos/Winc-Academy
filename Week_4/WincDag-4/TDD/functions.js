5642b5732efc573ea66d5236333452296bf7aa46
alex van den Bos <alex171293@hotmail.com>
2020-05-28 20:39:15 +0200

    initial commit  jest testing assignments

---
+++ Week_4/WincDag-4/TDD/functions.js
@@ -0,0 +1,32 @@
+// const addOne = (numbersArray) => {
+// 	let numbersArrayPlusOne = numbersArray.map(number => number + 1);
+// 	console.log(numbersArrayPlusOne)
+// 	return numbersArrayPlusOne;
+// }
+
+// module.exports = addOne;
+
+const functions = {
+    addOne: function(numbersArray) {
+		  let numbersArrayPlusOne = numbersArray.map(number => number + 1);
+      return numbersArrayPlusOne;
+    },
+    getWordLengths: function(someWords) {
+      let wordsLength = someWords.map(word => word.length);
+      return wordsLength;
+    },
+    findNeedle: function(someWords, filterword) {
+      let needleLocationIterativeVariable = someWords.indexOf(filterword)
+      return needleLocationIterativeVariable
+    }
+    // createUser: function() {
+    //   const user = { 
+    //     firstName: 'Brad' 
+    //   };
+    //   user['lastName'] = 'Traversy';
+    //   return user;
+    // }
+  }; 
+module.exports = functions;
+// const myArray = [31, 57, 12, 5];
+// console.log(functions.addOne(myArray))
---
+++ Week_4/WincDag-4/TDD/functions.test.js
@@ -0,0 +1,38 @@
+// const addOne = require("./addone.js")
+
+// test("Add 1 to each item in myArray", function() {
+//     const myArray = [31, 57, 12, 5];  
+//     const unchanged = [31, 57, 12, 5];
+    
+//     const expected = [32, 58, 13, 6];    
+//     const output = addOne(myArray);
+  
+//     expect(output).toEqual(expected);
+//     expect(myArray).toEqual(unchanged);
+//   });
+const functions = require('./functions.js')
+
+test("Add 1 to each item in myArray", function() {
+    const myArray = [31, 57, 12, 5];  
+    const unchanged = [31, 57, 12, 5];
+    
+    const expected = [32, 58, 13, 6];    
+    const output = functions.addOne(myArray);
+  
+    expect(output).toEqual(expected);
+    expect(myArray).toEqual(unchanged);
+});
+test("Display the length of the words from array words", function() {
+    const words = ["sun", "potato", "roundabout", "pizza"];
+    const expected = [3, 6, 10, 5];
+
+    const output = functions.getWordLengths(words);
+    expect(output).toEqual(expected);
+});
+test("Find the needle", function() {
+    const words = ["house", "train", "slide", "needle", "book"];
+    const expected = 3;
+  
+    const output = functions.findNeedle(words, "needle");
+    expect(output).toEqual(expected);
+  });
